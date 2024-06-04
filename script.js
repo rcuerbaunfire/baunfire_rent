e957514091975cb4cc162cbc06b2f5a4ba550e98
Rich Philben Cuer <shinigami.richi@gmail.com>
2024-06-05 07:15:32 +0800

    updates to layout

---
+++ script.js
@@ -0,0 +1,91 @@
+const leftArrow = `<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="16" cy="16" r="16" transform="matrix(-1 0 0 1 32 0)" fill="#4561EC"/><path d="M24 16L9.60001 16" stroke="#F6F6FF" stroke-width="2.00439"/><path d="M15.9996 9.6001L9.59961 16.0001L15.9996 22.4001" stroke="#F6F6FF" stroke-width="2.00439"/></svg>`;
+const rightArrow = `<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="16" cy="16" r="16" fill="#4561EC"/><path d="M8 16L22.4 16" stroke="#F6F6FF" stroke-width="2.00439"/><path d="M16.0004 9.6001L22.4004 16.0001L16.0004 22.4001" stroke="#F6F6FF" stroke-width="2.00439"/></svg>`;
+
+$(document).ready(function () {
+    function carousels() {
+        const carousels = $(".owl-carousel, .mobile-owl-carousel");
+        if (!carousels.length) return;
+
+        carousels.each(function () {
+            const self = $(this);
+
+            if (self.hasClass("tes-items")) {
+                const carouselInstance = self.owlCarousel({
+                    nav: true,
+                    smartSpeed: 1000,
+                    items: 1,
+                    loop: false,
+                    navRewind: false,
+                    dotsEach: true,
+                    navText: [leftArrow, rightArrow],
+                    margin: 60,
+                });
+
+                const dots = self.find(".owl-dot");
+                dots.each(function (index) {
+                    const dot = $(this);
+                    const num = index + 1;
+
+                    if (num == dots.length) {
+                        dot.children().remove();
+                        dot.html(`<span>${num}</span>/<span>${num}</span>`);
+                    } else {
+                        dot.children("span").text(num);
+                    }
+                });
+            } else if (self.hasClass("sol-items")) {
+                const carouselInstance = self.owlCarousel({
+                    nav: true,
+                    smartSpeed: 1000,
+                    loop: false,
+                    navRewind: false,
+                    dotsEach: true,
+                    navText: [leftArrow, rightArrow],
+                    responsive: {
+                        0: { autoWidth: false, margin: 16, items: 1 },
+                        768: { autoWidth: true, margin: 24 },
+                    },
+                });
+            } else if (self.hasClass("res-items")) {
+                const mm = gsap.matchMedia();
+                let carouselInstance = null;
+
+                mm.add(
+                    {
+                        isDesktop: `(min-width: 768px)`,
+                        isMobile: `(max-width: 767px)`,
+                    },
+                    (context) => {
+                        let { isDesktop, isMobile } = context.conditions;
+
+                        if (isMobile) {
+                            if (!self.hasClass("owl-loaded")) {
+                                carouselInstance = self.owlCarousel({
+                                    nav: false,
+                                    items: 1,
+                                    smartSpeed: 1000,
+                                    loop: false,
+                                    navRewind: false,
+                                    dotsEach: true,
+                                    margin: 16,
+                                });
+                            }
+                        }
+
+                        if (isDesktop) {
+                            if (carouselInstance) {
+                                carouselInstance.trigger(
+                                    "destroy.owl.carousel"
+                                );
+                                carouselInstance = null;
+                            }
+                        }
+
+                        return () => { };
+                    }
+                );
+            }
+        });
+    }
+    carousels();
+});
