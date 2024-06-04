7a28b5f30e924146e578066d8f9a51cf88b58b3d
Rich Philben Cuer <shinigami.richi@gmail.com>
2024-06-05 07:37:15 +0800

    updates to layout

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const leftArrow = `<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="16" cy="16" r="16" transform="matrix(-1 0 0 1 32 0)" fill="#4561EC"/><path d="M24 16L9.60001 16" stroke="#F6F6FF" stroke-width="2.00439"/><path d="M15.9996 9.6001L9.59961 16.0001L15.9996 22.4001" stroke="#F6F6FF" stroke-width="2.00439"/></svg>`;
-const rightArrow = `<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="16" cy="16" r="16" fill="#4561EC"/><path d="M8 16L22.4 16" stroke="#F6F6FF" stroke-width="2.00439"/><path d="M16.0004 9.6001L22.4004 16.0001L16.0004 22.4001" stroke="#F6F6FF" stroke-width="2.00439"/></svg>`;
-
 $(document).ready(function () {
     function carousels() {
         const carousels = $(".owl-carousel, .mobile-owl-carousel");
         if (!carousels.length) return;
+
+        const leftArrow = `<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="16" cy="16" r="16" transform="matrix(-1 0 0 1 32 0)" fill="#4561EC"/><path d="M24 16L9.60001 16" stroke="#F6F6FF" stroke-width="2.00439"/><path d="M15.9996 9.6001L9.59961 16.0001L15.9996 22.4001" stroke="#F6F6FF" stroke-width="2.00439"/></svg>`;
+        const rightArrow = `<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="16" cy="16" r="16" fill="#4561EC"/><path d="M8 16L22.4 16" stroke="#F6F6FF" stroke-width="2.00439"/><path d="M16.0004 9.6001L22.4004 16.0001L16.0004 22.4001" stroke="#F6F6FF" stroke-width="2.00439"/></svg>`;
 
         carousels.each(function () {
             const self = $(this);
@@ -87,5 +87,58 @@ $(document).ready(function () {
             }
         });
     }
+
+    function animatedContactHeading() {
+        const container = $(".ach-to-show-container");
+        if (!container.length) return;
+
+        container.each(function () {
+            const self = $(this);
+            const toExit = self.find(".ach-to-exit");
+            const toShowMain = self.find(".ach-to-show-main");
+            const toShowSecondary = self.find(".ach-to-show");
+
+            const timeline = gsap.timeline({
+                defaults: {
+                    ease: Power3.easeOut,
+                    overwrite: true
+                }
+            });
+
+            timeline
+            .fromTo(toExit, 
+                {
+                    clipPath: "inset(0 0 0 0)"
+                },
+                {
+                    clipPath: "inset(0 0 100% 0)",
+                    duration: 0.6,
+                },
+            )
+            .fromTo(toShowMain, 
+                {
+                    autoAlpha: 0
+                },
+                {
+                    autoAlpha: 1,
+                    duration: 0.4,
+                },
+                "<0.4"
+            )
+            .fromTo(toShowSecondary, 
+                {
+                    height: 0
+                },
+                {
+                    height: "auto",
+                    duration: 0.4,
+                    stagger: 0.14
+                },
+                "<0.3"
+            )
+        })
+    }
+
     carousels();
+    animatedContactHeading();
 });
